d450f8d847ccbdc25b30678bf66b852a87ec6d26
vungocthach <vungocthach1112@gmail.com>
2022-11-25 23:27:57 +0700

    fix

--- renderer.js
+++ renderer.js
@@ -3,7 +3,7 @@ const btn = document.getElementById('btn')
 const filePathElement = document.getElementById('filePath')
 information.innerText = `This app is using Chrome (v${electronAPI.versions.chrome()}), Node.js (v${electronAPI.versions.node()}), and Electron (v${electronAPI.versions.electron()})`;
 
-btn.addEventListener('click', async () => {
-    const filePath = await electronAPI.ping()
-    filePathElement.innerText = filePath
-})
+// btn.addEventListener('click', async () => {
+//     const filePath = await electronAPI.ping()
+//     filePathElement.innerText = filePath
+// })
